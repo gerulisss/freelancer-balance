3a1d3760fa64e4e5891518e6d22f86a144c80cb0
gerulisss <rokodarbinis@gmail.com>
2019-10-03 20:48:23 +0300

    Final code

---
+++ js/actions.js
@@ -0,0 +1,1 @@
+renderMetai( months );
---
+++ js/function.js
@@ -0,0 +1,10 @@
+
+function renderMetai( data ) {
+    console.log(data);
+
+    HTML += `<div class="table-content"></div>
+        <div class="table-head"></div>
+        <img src="${obj.photo}" title="alt"> </a>`;
+
+    return document.getElementById('months').innerHTML = HTML;
+}
